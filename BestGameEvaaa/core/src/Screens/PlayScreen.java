047aa5d72d2309fa815cfc95ad89791a2cd4f5d9
Unrealdst <unrealdst@gmail.com>
2015-05-19 11:22:33 +0200

    Set up screen change

--- BestGameEvaaa/core/src/Screens/PlayScreen.java
+++ BestGameEvaaa/core/src/Screens/PlayScreen.java
@@ -1,28 +1,36 @@
 package Screens;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.BestGameEvaa;
 
-public class PlayScreen implements Screen, InputProcessor, ApplicationListener {
-
-	@Override
-	public void create() {
-		// TODO Auto-generated method stub
+public class PlayScreen implements Screen {
+	
+	SpriteBatch batch;
+	Texture img;
+	BestGameEvaa game;
+	
+	public PlayScreen(BestGameEvaa game){
+		this.game = game;
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
 		
-	}
+	}	
+
+
+
 
 	@Override
 	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stu	
 	}
 
-	@Override
-	public void render() {
-		// TODO Auto-generated method stub
-		
-	}
+
 
 	@Override
 	public void pause() {
@@ -41,64 +49,24 @@ public class PlayScreen implements Screen, InputProcessor, ApplicationListener {
 		// TODO Auto-generated method stub
 		
 	}
-
-	@Override
-	public boolean keyDown(int keycode) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean keyUp(int keycode) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean keyTyped(char character) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean mouseMoved(int screenX, int screenY) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean scrolled(int amount) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
 	@Override
 	public void render(float delta) {
-		// TODO Auto-generated method stub
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
+		batch.begin();	
+		batch.draw(img, 0, 0);
+		batch.end();		
 	}
 
 	@Override
 	public void show() {
-		// TODO Auto-generated method stub
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		batch.begin();	
+		batch.draw(img, 0, 0);
+		batch.end();		
 		
 	}
 
--- BestGameEvaaa/core/src/com/mygdx/game/BestGameEvaa.java
+++ BestGameEvaaa/core/src/com/mygdx/game/BestGameEvaa.java
@@ -1,19 +1,30 @@
 package com.mygdx.game;
 
+import Screens.PlayScreen;
+
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class BestGameEvaa extends ApplicationAdapter {
+public class BestGameEvaa extends Game {
 	SpriteBatch batch;
 	Texture img;
+	public PlayScreen play;
+	
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		play = new PlayScreen(this);
+		
+		setScreen(play);
+		play.show();
+		
 	}
 
 	@Override
@@ -23,5 +34,6 @@ public class BestGameEvaa extends ApplicationAdapter {
 		batch.begin();
 		//batch.draw(img, 0, 0);
 		batch.end();
+		
 	}
 }
