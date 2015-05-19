2fba56511bf32a9ca8bfbd5fd5cc4e1b9a615c7c
Exik <extreme0312@gmail.com>
2015-05-19 12:35:03 +0200

    no message

--- BestGameEvaaa/core/src/com/mygdx/game/BestGameEvaa.java
+++ BestGameEvaaa/core/src/com/mygdx/game/BestGameEvaa.java
@@ -1,30 +1,19 @@
 package com.mygdx.game;
 
-import Screens.PlayScreen;
-
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class BestGameEvaa extends Game {
+public class BestGameEvaa extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	public PlayScreen play;
-	
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
-		play = new PlayScreen(this);
-		
-		setScreen(play);
-		play.show();
-		
 	}
 
 	@Override
@@ -32,8 +21,7 @@ public class BestGameEvaa extends Game {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		//batch.draw(img, 0, 0);
+		batch.draw(img, 0, 0);
 		batch.end();
-		
 	}
 }
