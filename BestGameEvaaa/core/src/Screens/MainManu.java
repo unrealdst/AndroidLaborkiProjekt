f5f4e9e31c79b986079c5eb7b6aaa83068e463ea
Exik <extreme0312@gmail.com>
2015-05-22 21:16:56 +0200

    Main menu background(logo) added + some changes in button style(just test)

--- BestGameEvaaa/core/src/Screens/MainManu.java
+++ BestGameEvaaa/core/src/Screens/MainManu.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -20,6 +22,8 @@ public class MainManu extends PlayScreenFields implements Screen {
 
 	public MainManu(BestGameEvaa game) {
 		this._game = game;
+		batch = new SpriteBatch();
+		background = new Texture("logo.png");
 		stage = new Stage();
 
 		createNewGameButton();
@@ -28,7 +32,7 @@ public class MainManu extends PlayScreenFields implements Screen {
 
 	private void createExitGameButton() {
 		exitButton = new TextButton("Exit", _game.skin);
-		exitButton.setBounds(200, 10, 100, 100);
+		exitButton.setBounds(Gdx.graphics.getWidth()/2 - 100, Gdx.graphics.getHeight()/2 - 175, 200, 100);
 		exitButton.addListener(new InputListener() {
 			public boolean touchDown(InputEvent event, float x, float y,
 					int pointer, int button) {
@@ -43,7 +47,7 @@ public class MainManu extends PlayScreenFields implements Screen {
 
 	private void createNewGameButton() {
 		newGameButton = new TextButton("New Game", _game.skin);
-		newGameButton.setBounds(10, 10, 100, 100);
+		newGameButton.setBounds(Gdx.graphics.getWidth()/2 - 100, Gdx.graphics.getHeight()/2 - 45, 200, 100);
 		newGameButton.addListener(new InputListener() {
 			public boolean touchDown(InputEvent event, float x, float y,
 					int pointer, int button) {
@@ -60,6 +64,10 @@ public class MainManu extends PlayScreenFields implements Screen {
 		Gdx.input.setInputProcessor(stage);
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		batch.begin();
+		batch.draw(background, 0, 0);
+		batch.end();
 		
 		Gdx.input.setInputProcessor(stage);
 		stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
@@ -99,7 +107,8 @@ public class MainManu extends PlayScreenFields implements Screen {
 	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
-
+		stage.dispose();
+		batch.dispose();
 	}
 
 }
